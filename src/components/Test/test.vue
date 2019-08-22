0c28272e6a0fee08d3c7d5d3dc5cb6af6cf88e42
Cenergy <673598118@qq.com>
2019-08-23 00:19:46 +0800

    md

--- src/components/Test/test.vue
+++ src/components/Test/test.vue
@@ -1,15 +1,33 @@
 <template>
   <div>
-      hello
+    <input type="button" value="happy" @click="lang" />
+    <input type="button" value="stop" @click="stop" />
+    <h4>{{msg}}</h4>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  
-
+  data() {
+    return {
+      msg: 'hello world',
+      intervalID: null,
+    };
+  },
+  methods: {
+    lang() {
+      if (this.intervalID) return;
+      this.intervalID = setInterval(() => {
+        const start = this.msg.substring(0, 1);
+        const end = this.msg.substring(1);
+        this.msg = end + start;
+      }, 400);
+    },
+    stop() {
+      clearInterval(this.intervalID);
+      this.intervalID = null;
+    },
+  },
 };
 </script>
 
