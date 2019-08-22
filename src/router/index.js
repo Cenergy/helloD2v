c9de4490af86e540f92366d2c33ac30752571ac0
Cenergy <673598118@qq.com>
2019-08-22 23:46:38 +0800

    test

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,34 @@
-import Vue from "vue";
-import Router from "vue-router";
+import Vue from 'vue';
+import Router from 'vue-router';
 
 Vue.use(Router);
 
-import hello from "../components/hello/hello.vue";
-import world from "../components/world/world.vue";
-import About from "../pages/About/About.vue";
-import Home from "../pages/Home/Home.vue";
+import hello from '../components/hello/hello.vue';
+import world from '../components/world/world.vue';
+import About from '../pages/About/About.vue';
+import Home from '../pages/Home/Home.vue';
+
+import test from './test';
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   routes: [
     {
-      path: "/",
-      component: Home
+      path: '/',
+      component: Home,
     },
     {
-      path: "/hello",
-      component: hello
+      path: '/hello',
+      component: hello,
     },
     {
-      path: "/world",
-      component: world
+      path: '/world',
+      component: world,
     },
     {
-      path: "/about",
-      component: About
-    }
-  ]
+      path: '/about',
+      component: About,
+    },
+    test,
+  ],
 });
